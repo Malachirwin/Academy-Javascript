68a71d1ed7ca20c0e1654a8e984a76204974033c
Malachi Irwin <malachi@theirwins.ws>
2019-06-11 13:28:00 -0500

    Added goFishController class

---
+++ jasmine/spec/goFishControllerSpec.js
@@ -0,0 +1,5 @@
+describe("GoFishController", () => {
+  it("gets login", () => {
+    const login = GoFishController
+  });
+});
---
+++ jasmine/src/goFishController.js
@@ -0,0 +1,18 @@
+class GoFishController {
+  container() {
+    return document.getElementById('main')
+  }
+
+  login() {
+    const view = new LoginView(this.startGame.bind(this))
+    view.draw(this.container())
+  }
+
+  startGame(name) {
+    const game = new Game(name)
+    const view = new GameView(game)
+    view.draw(this.container())
+  }
+}
+window.controller = new GoFishController();
+window.onload = controller.login.bind(window.controller)
